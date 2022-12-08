78b0679dad3b0131cde209b46ca5b7fe038fbdaf
Shrenuka <31723435+Shrenuka@users.noreply.github.com>
2022-12-08 13:57:21 +0530

    second commit

--- src/String/StringAreImmutable.java
+++ src/String/StringAreImmutable.java
@@ -12,24 +12,36 @@ public class StringAreImmutable {
 		
 		System.out.println(s1+" "+s2);
 		
+		System.out.println("**************");
+		
 		s1.concat(s2);
 		System.out.println(s1);
 		
+		System.out.println("**************");
+				
 		s1=s1.concat(" "+s2);
 		System.out.println(s1);
+		
+		System.out.println("**************");
 		
 		int val = 64;
 		float ff = 55.44f;
 		String s3 = "Shivaji";
 		
-		//s3 = s3.concat(value+ff); //error--> we should add " " before adding different data types
+		//s3 = s3.concat(value+ff); //error--> we should add "" before adding different data types
 		s3 = s3.concat(""+val+ff);
 		System.out.println(s3);
 		
+		System.out.println("**************");
+		
 		System.out.println(s2+val+ff);
+		
+		System.out.println("**************");
 		
 		String s4 = s3.toString();
 		System.out.println(s4);
+		
+		System.out.println("**************");
 		
 		s4 = (""+val).toString();
 		System.out.println(s4);
